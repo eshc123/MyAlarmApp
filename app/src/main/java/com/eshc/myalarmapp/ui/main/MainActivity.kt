a86e4fb8417998d05967aab4b05642dd5c0e145e
Jung SeokJoon <eshc.joon@gmail.com>
2022-09-06 18:18:27 +0900

    [FEATURE] #6 알람 추가 화면 ViewModel 연동

--- app/src/main/java/com/eshc/myalarmapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/eshc/myalarmapp/ui/main/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.eshc.myalarmapp.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.eshc.myalarmapp.R
 import com.eshc.myalarmapp.databinding.ActivityMainBinding
 import com.eshc.myalarmapp.ui.adapter.AlarmAdapter
+import com.eshc.myalarmapp.ui.detail.DetailActivity
 
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding
+
+    private val viewModel : MainViewModel by viewModels()
 
     private val alarmAdapter : AlarmAdapter by lazy {
         AlarmAdapter()
@@ -24,12 +29,21 @@ class MainActivity : AppCompatActivity() {
         )
 
         initRecyclerView()
+        initFab()
     }
 
     private fun initRecyclerView(){
         binding?.rvAlarm?.let {
             it.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
             it.adapter = alarmAdapter
+        }
+    }
+
+    private fun initFab(){
+        binding?.fbRegister?.let {
+            it.setOnClickListener {
+                startActivity(Intent(this,DetailActivity::class.java))
+            }
         }
     }
 }
